cc49ec05205beb2ec332d87e6a479de313df66de
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-06-03 16:34:08 +0100

    Initial commit

---
+++ src/test/scala/ExampleTest.scala
@@ -0,0 +1,7 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class ExampleTest extends FlatSpec with Matchers {
+    "List with no elements" should "be Nil" in {
+        true should be(true)
+    }
+}
