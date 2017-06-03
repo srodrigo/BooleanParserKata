2c8acad6335dc74e43dba00a0baaf89875b0650b
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-06-03 18:53:47 +0100

    Refactor: create Boolean Parser and AST
    Separate AST and evaluation

--- src/main/scala/BooleanCalculator.scala
+++ src/main/scala/BooleanCalculator.scala
@@ -1,7 +1,17 @@
 object BooleanCalculator {
 
     def evaluate(booleanExpr: String): Boolean = {
-        val token = BooleanLexer(booleanExpr).right.get
-        BooleanParser(token)
+        val ast: Either[Object, BooleanAST] = for {
+            token <- BooleanLexer(booleanExpr).right
+            ast <- BooleanParser(token).right
+        } yield ast
+
+        eval(ast.right.get)
+    }
+
+    private def eval(ast: BooleanAST): Boolean = ast match {
+        case BooleanValue(value) =>
+            if (value == "T") true
+            else false
     }
 }
--- src/main/scala/BooleanParser.scala
+++ src/main/scala/BooleanParser.scala
@@ -1,10 +1,16 @@
+import scala.util.parsing.combinator.Parsers
 
-object BooleanParser {
-    def apply(token: Token): Boolean = {
+object BooleanParser extends Parsers {
+    override type Elem = Token
+
+    def apply(token: Token): Either[BooleanParserError, BooleanAST] = {
         token match {
-            case BOOLEAN_VAL(booleanVal) =>
-                if (booleanVal.equals("T")) true
-                else false
+            case BOOLEAN_VAL(booleanVal) => Right(BooleanValue(booleanVal))
         }
     }
 }
+
+sealed trait BooleanAST
+final case class BooleanValue(str: String) extends BooleanAST
+
+trait BooleanParserError
