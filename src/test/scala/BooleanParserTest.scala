ca4d81568689d185a0f0671e3591692ce1ebc287
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-06-04 14:08:41 +0100

    Add passing unit test

--- src/test/scala/BooleanParserTest.scala
+++ src/test/scala/BooleanParserTest.scala
@@ -13,4 +13,8 @@ class BooleanParserTest extends FlatSpec with Matchers {
     "NOT T" should "be Boolean Value F" in {
         BooleanParser(List(NEGATION_OP, BOOLEAN_VAL(true))).right.get should be(BooleanValue(false))
     }
+
+    "NOT F" should "be Boolean Value T" in {
+        BooleanParser(List(NEGATION_OP, BOOLEAN_VAL(false))).right.get should be(BooleanValue(true))
+    }
 }
