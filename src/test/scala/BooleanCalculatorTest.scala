0ad2290d89c729dfb24fc33b6fa07223234abb7e
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-06-21 09:39:35 +0100

    Remove redundant acceptance tests

--- src/test/scala/BooleanCalculatorTest.scala
+++ src/test/scala/BooleanCalculatorTest.scala
@@ -3,26 +3,6 @@ import BooleanCalculator.{evaluate, astAsString}
 
 class BooleanCalculatorTest extends FlatSpec with Matchers {
 
-    "T" should "be true" in {
-        evaluate("T") should be(true)
-    }
-
-    "NOT T" should "be false" in {
-        evaluate("NOT T") should be(false)
-    }
-
-    "T AND F" should "be false" in {
-        evaluate("T AND F") should be(false)
-    }
-
-    "F OR T" should "be true" in {
-        evaluate("F OR T") should be(true)
-    }
-
-    "NOT F AND T" should "be true" in {
-        evaluate("NOT F AND T") should be(true)
-    }
-
     "NOT T AND F AND T OR T" should "be true" in {
         evaluate("NOT T AND F AND T OR T") should be(true)
     }
